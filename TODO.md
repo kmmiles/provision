c3c73bfa419cee26c443a447fb2e895544c851c9
kmmiles <kenneth.mmiles@gmail.com>
2021-02-26 23:07:56 -0700

    phase 1 of google style updates

---
+++ TODO.md
@@ -0,0 +1,25 @@
+# TODO
+
+## google style guide
+
+  - Every file must have a top-level comment including a brief overview of its contents. A copyright notice and author information are optional.
+  - Any function that is not both obvious and short must be commented. Any function in a library must be commented regardless of length or complexity.
+
+```bash
+#######################################
+# Get configuration directory.
+# Globals:
+#   SOMEDIR
+# Arguments:
+#   None
+# Outputs:
+#   Writes location to stdout
+#######################################
+function get_dir() {
+  echo "${SOMEDIR}"
+}
+```
+  - Comment tricky, non-obvious, interesting or important parts of your code.
+
+
+
---
+++ lib.sh
@@ -0,0 +1,435 @@
+#!/bin/bash
+
+# library of common things used by scripts 
+
+################################################################################
+# Set and export global variables
+#
+# Globals:
+#   TEXT_BOLD, TEXT_RED, TEXT_GREEN, TEXT_YELLOW, TEXT_BLUE, TEXT_NORMAL,
+#   __STATUS_MSG, __LOGFILE, __SOURCE, __DIR, __FILE, __ROOT,
+#   DOWNLOADS, IS_UBUNTU, IS_DEBIAN, IS_WSL
+# Arguments:
+#   None
+################################################################################
+export_globals() {
+  # message that is set/used by status_* functions
+  __STATUS_MSG=
+  export __STATUS_MSG
+
+  # log_* functions write to LOGFILE if set (instead of stdout/err)
+  __LOGFILE=
+  export __LOGFILE
+
+  # determine and export:
+  #
+  #   __SOURCE: Who sourced this library? "script" or "shell".
+  #   __DIR: Directory of the script that sourced this library. 
+  #   __FILE: Filename of the script that sourced this library.
+  #   __ROOT: Parent directory of the script that sourced this library.
+  if [[ -n "${BASH_SOURCE[1]:-}" ]]; then
+    __SOURCE="script"
+    __DIR="$(cd "$(dirname "${BASH_SOURCE[1]}")" && pwd)"
+    __FILE="${__DIR}/$(basename "${BASH_SOURCE[1]}")"
+    __ROOT="$(cd "$(dirname "${__DIR}")" && pwd)"
+  else
+    __SOURCE="shell"
+    __DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+    __FILE="${__DIR}/$(basename "${BASH_SOURCE[0]}")"
+    __ROOT="${__DIR}"
+  fi
+  export __DIR __FILE __ROOT __SOURCE
+
+  # basic text colors
+  TEXT_BOLD=$(tput bold)
+  TEXT_RED=$(tput setaf 1)
+  TEXT_GREEN=$(tput setaf 2)
+  TEXT_YELLOW=$(tput setaf 3)
+  TEXT_BLUE=$(tput setaf 4)
+  TEXT_NORMAL=$(tput sgr0)
+  export TEXT_BOLD TEXT_RED TEXT_GREEN TEXT_YELLOW TEXT_BLUE TEXT_NORMAL
+
+  # files downloaded with `dl()` are stored here
+  DOWNLOADS="$HOME/Downloads"
+  export DOWNLOADS
+
+  # determine some info about the platform
+  IS_UBUNTU=$(grep -q "ID=ubuntu" /etc/os-release && echo true || echo false)
+  IS_DEBIAN=$(grep -q "ID=debian" /etc/os-release && echo true || echo false)
+  IS_WSL=$(command -v "wslpath" > /dev/null 2>&1 && echo true || echo false)
+  export IS_UBUNTU IS_DEBIAN IS_WSL
+}
+
+################################################################################
+# Set script options
+#
+# Globals:
+#   __SOURCE
+#   DEBUG
+# Arguments:
+#   None
+################################################################################
+set_options() {
+  if [[ "$__SOURCE" == "script" ]]; then
+    set -o errexit
+    set -o pipefail
+    set -o nounset
+  fi
+
+
+  if [[ "${DEBUG:-}" ]]; then
+    echo "*** Debug mode enabled ***"
+    set -o xtrace
+  else
+    set +o xtrace
+  fi
+}
+
+################################################################################
+# Functions to print a message in various colors
+#
+# Globals:
+#   None
+# Arguments:
+#   The message
+################################################################################
+msg()         { printf "%s\n" "${1}" ; }
+red_msg()     { printf "%s\n" "${TEXT_BOLD}${TEXT_RED}${1}${TEXT_NORMAL}" ; }
+green_msg()   { printf "%s\n" "${TEXT_BOLD}${TEXT_GREEN}${1}${TEXT_NORMAL}" ; }
+yellow_msg()  { printf "%s\n" "${TEXT_BOLD}${TEXT_YELLOW}${1}${TEXT_NORMAL}" ; }
+blue_msg()    { printf "%s\n" "${TEXT_BOLD}${TEXT_BLUE}${1}${TEXT_NORMAL}" ; }
+
+################################################################################
+# Sets log* functions to write to a file instead of stdout/stderr
+#
+# Globals:
+#   LOGFILE
+# Arguments:
+#   None
+################################################################################
+log_use_file() {
+  local logrel
+
+  logrel="logs/$(basename "$0").log"
+  LOGFILE="${__ROOT}/$logrel"
+
+  mkdir -p "${__ROOT}/logs"
+  rm -f "$LOGFILE"
+  touch "$LOGFILE"
+}
+
+################################################################################
+# Print prefix string of log messages (program named wrapped in brackets).
+# Globals:
+#   SOURCE
+# Arguments:
+#   None
+# Outputs:
+#   Prefix string or empty if not a script    
+################################################################################
+log_get_title() {
+  if [[ "$__SOURCE" == "script" ]];  then
+    echo "[$(basename "$0")] "
+  else
+    echo ""
+  fi
+}
+
+################################################################################
+# Print plain message to stdout (or LOGFILE)
+# Globals:
+#   LOGFILE
+# Arguments:
+#   The message
+################################################################################
+log_msg() {
+  if [[ "$LOGFILE" != "" ]]; then
+    msg "$(log_get_title)${1}" >> "$LOGFILE"
+  else
+    msg "$(log_get_title)${1}"
+  fi
+}
+
+################################################################################
+# Print red error message to stderr (or LOGFILE)
+# Globals:
+#   LOGFILE
+# Arguments:
+#   The message
+################################################################################
+log_err() {
+  if [[ "$LOGFILE" != "" ]]; then
+    msg "$(log_get_title)${1}" >> "$LOGFILE"
+  else
+    >&2 red_msg "$(log_get_title)${1}"
+  fi
+}
+
+################################################################################
+# Print yellow warning message to stderr (or LOGFILE)
+# Globals:
+#   LOGFILE
+# Arguments:
+#   The message
+################################################################################
+log_warn() {
+  if [[ "$LOGFILE" != "" ]]; then
+    msg "$(log_get_title)${1}" >> "$LOGFILE"
+  else
+    >&2 yellow_msg "$(log_get_title)${1}"
+  fi
+}
+
+################################################################################
+# Print blue info message to stderr (or LOGFILE)
+# Globals:
+#   LOGFILE
+# Arguments:
+#   The message
+################################################################################
+log_info() {
+  if [[ "$LOGFILE" != "" ]]; then
+    msg "$(log_get_title)${1}" >> "$LOGFILE"
+  else
+    >&2 blue_msg "$(log_get_title)${1}"
+  fi
+}
+
+################################################################################
+# Print and save a status message for a slackware-esquee OK/FAIL line.
+# Globals:
+#   STATUS_MSG
+# Arguments:
+#   The message
+# Oututs:
+#   The message
+################################################################################
+status_msg() {
+  STATUS_MSG="$1"
+  printf '%s' "$STATUS_MSG"
+}
+
+################################################################################
+# Prints green [OK] after the status message
+# Arguments:
+#   None
+# Outputs:
+#   See description
+################################################################################
+status_ok() {
+  local len
+  len=$(status_msglen)
+  printf "%${len}s\\n" "[${TEXT_BOLD}${TEXT_GREEN} OK ${TEXT_NORMAL}]"
+}
+
+################################################################################
+# Prints red [FAIL] after the status message
+# Arguments:
+#   None
+# Outputs:
+#   See description
+################################################################################
+status_fail() {
+  local len
+  len=$(status_msglen)
+  printf "%${len}s\\n" "[${TEXT_BOLD}${TEXT_RED}FAIL${TEXT_NORMAL}]"
+}
+
+################################################################################
+# Used for justifying the OK/FAIL text after each status message
+# Globals:
+#   STATUS_MSG
+# Arguments:
+#   None
+# Outputs:
+#   console width minus the current status message
+################################################################################
+status_msglen() {
+  local msglen="${#STATUS_MSG}"
+  local columns=
+  columns=$(tput cols)
+  echo "$((columns-msglen))"
+}
+
+################################################################################
+# require_ functions to be placed at top of scripts
+################################################################################
+
+require_non_root() {
+  if [[ "$(id -u)" == "0" ]]; then
+    redmsg "ERROR: Do not run this script as root."
+    return 1
+  fi
+}
+
+require_root() {
+  if [[ "$(id -u)" != "0" ]]; then
+    redmsg "ERROR: please run as normal user w/ sudo"
+    return 1
+  fi
+
+  if [[ -z "$SUDO_USER" ]]; then
+    redmsg "ERROR: please run as normal user w/ sudo"
+    return 1
+  fi
+}
+
+require_wsl() {
+  if ! $IS_WSL; then
+    redmsg "ERROR: This script requires WSL2."
+    return 1
+  fi
+}
+
+################################################################################
+# chkpath: verify all bins passed are in $PATH
+################################################################################
+
+chkpath() {
+  # no programs specified
+  if [[ $# -eq 0 ]]; then
+    return 1
+  fi
+
+  for prog in "$@"; do
+    command -v "$prog" > /dev/null 2>&1 || return 1
+  done
+}
+
+################################################################################
+# chkpkg: verify all packages passed are installed
+################################################################################
+
+chkpkg() {
+  # no packages specified
+  if [[ $# -eq 0 ]]; then
+    return 1
+  fi
+
+  for pkg in "$@"; do
+    dpkg -V "$pkg" > /dev/null 2>&1 || return 1
+  done
+}
+
+################################################################################
+# dl: Downloads url passed and prints the local path when succesful
+################################################################################
+
+dl() {
+  local url filepath filename
+
+  url="$1"
+  if [[ -z "$url" ]]; then
+    return 1
+  fi
+
+  filename=$(basename "$url")
+  filepath="$DOWNLOADS/$filename"
+
+  # destination directory must exist
+  if [[ ! -d "$DOWNLOADS" ]]; then
+    log_err "Directory does not exist \"$DOWNLOADS\"."
+    return 1 
+  fi
+
+  # download file
+  if [[ ! -f "$filepath" ]]; then
+    log_info "Downloading \"$url\" to \"$DOWNLOADS\"..."
+    if ! curl -sfL -o "$filepath" "$url" > /dev/null; then
+      log_err "Failed to download \"$url\"."
+      return 1
+    fi
+  fi
+
+  # print path of downloaded file
+  echo "$filepath"
+}
+
+################################################################################
+# get the windows username (only applies to WSL2)
+################################################################################
+get_win_user() {
+  local win_drive="$1"
+  declare -a invalid_users
+  invalid_users=( Administrator Default DefaultAccount Guest Public WDAGUtilityAccount )
+  find "/mnt/$win_drive/Users" -mindepth 1 -maxdepth 1 -type d |
+    while read -r line; do
+      user="$(basename "$line")"
+      if [[ ${invalid_users[*]} =~ ${user} ]]; then
+        continue
+      fi
+    
+      echo "$user"
+      break
+    done
+}
+
+################################################################################
+# "main"
+################################################################################
+# message that is set/used by status_* functions
+__STATUS_MSG=
+export __STATUS_MSG
+
+# log_* functions write to LOGFILE if set (instead of stdout/err)
+__LOGFILE=
+export __LOGFILE
+
+# determine and export:
+#
+#   __SOURCE: Who sourced this library? "script" or "shell".
+#   __DIR: Directory of the script that sourced this library. 
+#   __FILE: Filename of the script that sourced this library.
+#   __ROOT: Parent directory of the script that sourced this library.
+if [[ -n "${BASH_SOURCE[1]:-}" ]]; then
+  __SOURCE="script"
+  __DIR="$(cd "$(dirname "${BASH_SOURCE[1]}")" && pwd)"
+  __FILE="${__DIR}/$(basename "${BASH_SOURCE[1]}")"
+  __ROOT="$(cd "$(dirname "${__DIR}")" && pwd)"
+else
+  __SOURCE="shell"
+  __DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+  __FILE="${__DIR}/$(basename "${BASH_SOURCE[0]}")"
+  __ROOT="${__DIR}"
+fi
+export __DIR __FILE __ROOT __SOURCE
+
+# basic text colors
+TEXT_BOLD=$(tput bold)
+TEXT_RED=$(tput setaf 1)
+TEXT_GREEN=$(tput setaf 2)
+TEXT_YELLOW=$(tput setaf 3)
+TEXT_BLUE=$(tput setaf 4)
+TEXT_NORMAL=$(tput sgr0)
+export TEXT_BOLD TEXT_RED TEXT_GREEN TEXT_YELLOW TEXT_BLUE TEXT_NORMAL
+
+# files downloaded with `dl()` are stored here
+DOWNLOADS="$HOME/Downloads"
+export DOWNLOADS
+
+# determine some info about the platform
+IS_UBUNTU=$(grep -q "ID=ubuntu" /etc/os-release && echo true || echo false)
+IS_DEBIAN=$(grep -q "ID=debian" /etc/os-release && echo true || echo false)
+IS_WSL=$(command -v "wslpath" > /dev/null 2>&1 && echo true || echo false)
+export IS_UBUNTU IS_DEBIAN IS_WSL
+
+# TODO: main function
+if $IS_WSL; then
+  path="$HOME/winhome"
+
+  if [[ ! -L "$path" || ! -d "$path" ]]; then
+    echo "Creating winhome link in $HOME/winhome..."
+    win_drive="$(basename "$(grep drvfs /proc/self/mounts | head -n 1 | cut -d' ' -f 2)")"
+    win_user="$(get_win_user "$win_drive")"
+    win_home="/mnt/$win_drive/Users/$win_user"
+    if [ -d "$win_home" ]; then
+      ln -vsf "$win_home" "$HOME/winhome"
+    fi
+
+    for d in Downloads Music; do
+      if [[ ! -d "$HOME/$d" ]]; then
+        ln -vsf "$win_home/$d" "$HOME/$d"
+      fi
+    done
+  fi
+fi
