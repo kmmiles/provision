5ef91f31cb24b28aa74e043469de7b871593803e
kmmiles <kenneth.mmiles@gmail.com>
2021-03-26 18:46:47 -0600

    update

--- dotfiles/.config/bashrc.d/path.sh
+++
@@ -1,8 +0,0 @@
-PONYBIN=$HOME/.local/share/ponyup/bin
-FLUTTERBIN=$HOME/Code/flutter/bin
-EMACSBIN=~/.emacs.d/bin
-PATH="$HOME/.local/bin:$EMACSBIN:$FLUTTERBIN:$PONYBIN:$PATH"
-
-GOPATH=$HOME/go
-
-export PATH GOPATH
--- dotfiles/.config/bashrc.d/wsl2.sh
+++
@@ -1,5 +0,0 @@
-# x11 for wsl2 
-if command -v wslpath > /dev/null 2>&1; then
-  DISPLAY=$(grep nameserver /etc/resolv.conf | awk '{print $2; exit;}'):0.0
-  export DISPLAY
-fi
